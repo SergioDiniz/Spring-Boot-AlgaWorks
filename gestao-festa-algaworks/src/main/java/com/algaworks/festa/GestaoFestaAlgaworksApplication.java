20d5bb64f5aa3a86f74681adccbd76aeaa0720a5
Sergio Diniz <sergiodinizsh@gmail.com>
2017-01-21 08:18:05 -0300

    init

---
+++ gestao-festa-algaworks/src/main/java/com/algaworks/festa/GestaoFestaAlgaworksApplication.java
@@ -0,0 +1,12 @@
+package com.algaworks.festa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFestaAlgaworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFestaAlgaworksApplication.class, args);
+	}
+}
