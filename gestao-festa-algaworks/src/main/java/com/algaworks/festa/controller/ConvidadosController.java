79ae56c8293c015340a646a4b67647502af558e5
Sergio Diniz <sergiodinizsh@gmail.com>
2017-01-21 12:11:51 -0300

    adicionando novo usuario e usando banco postgres no lugador do h2

--- gestao-festa-algaworks/src/main/java/com/algaworks/festa/controller/ConvidadosController.java
+++ gestao-festa-algaworks/src/main/java/com/algaworks/festa/controller/ConvidadosController.java
@@ -3,24 +3,35 @@ package com.algaworks.festa.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.algaworks.festa.model.Usuario;
 import com.algaworks.festa.repository.UsuarioRepository;
 
 @Controller
+@RequestMapping("/convidados")
 public class ConvidadosController {
 
 	@Autowired
 	UsuarioRepository usuarioRepository;
 	
 	
-	@GetMapping("/convidados")
+	@GetMapping
 	public ModelAndView listar(){
 		
 		ModelAndView view = new ModelAndView("ListaCondidados");
 		view.addObject("convidados", usuarioRepository.findAll());
+		view.addObject(new Usuario());
 		
 		return view;
+	}
+	
+	@PostMapping
+	public String salvar(Usuario usuario){
+		this.usuarioRepository.save(usuario);
+		return "redirect:/convidados";
 	}
 	
 }
